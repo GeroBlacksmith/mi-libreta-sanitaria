b0e2d10b53aa866b20542fd48b94ea6af3fc85f3
geroblksmz <gerardoherrera87@yahoo.com.ar>
2020-07-25 20:00:11 -0300

    Pets redefined

--- src/pets/schemas/pets.schema.ts
+++ src/pets/schemas/pets.schema.ts
@@ -3,6 +3,8 @@ const Schema = mongoose.Schema;
 export const PetsSchema = new mongoose.Schema({
     name: String,
     birthDate: Date,
+    specie: String,
+    gender: String,
     race: String,
     color: String,
     propietary: { type: Schema.Types.ObjectId, ref: 'Person' },
